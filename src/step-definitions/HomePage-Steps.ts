4ecc1220b7ff8a23a1ab7d62f9ab2f3ceb7899d2
LA-DESKTOP\lasgi <lasgill@gmail.com>
2025-10-17 13:12:13 -0400

    added util cucumber default time out

--- src/step-definitions/HomePage-Steps.ts
+++ src/step-definitions/HomePage-Steps.ts
@@ -1,9 +1,23 @@
 import { Given, When, Then } from "@cucumber/cucumber";
+import { Browser, chromium } from "playwright";
+import { Page } from "playwright";
 
-Given("I navigate to the webdriveruniversity homepage", async () => {
-    console.log("Navigating to the homepage");
+let browser: Browser; //represents the browser instance e.g. Chrome, Firefox, etc. open by Playwright
+let context: any; //represents a browser context (a separate browsing session);  Each contxt has its own cookies, cache, storage, etc.
+let page: Page; //represents a single tab or page in the browser context 
+
+const url = "https://www.webdriveruniversity.com";
+
+Given('I navigate to the webdriveruniversity homepage', async () => {
+    //setup browser instance:
+    browser = await chromium.launch({ headless: false }); 
+    context = await browser.newContext({ viewport: { width: 1920, height: 1080 }});
+    page = await context.newPage();
+
+    await page.goto(url);  
 });
 
-When("I click on the contact us button", async () => {
-    console.log("Clicking on the contact us button");
+When('I click on the contact us button', {timeout: 60 * 1000}, async () => {
+    // await page.pause(); // Pause for debugging
+    await page.click("#contact-us");
 });
---
+++ src/utils/cucumber-timeout.ts
@@ -0,0 +1,3 @@
+import { setDefaultTimeout } from "@cucumber/cucumber"; 
+
+setDefaultTimeout(6000); // Set default step timeout to 60 seconds
