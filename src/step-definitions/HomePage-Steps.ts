e067324b0f98e3ef7f77b0a2460fdaf0e5a22ba1
LA-DESKTOP\lasgi <lasgill@gmail.com>
2025-10-17 12:27:44 -0400

    added home page steps

---
+++ src/step-definitions/HomePage-Steps.ts
@@ -0,0 +1,9 @@
+import { Given, When, Then } from "@cucumber/cucumber";
+
+Given("I navigate to the webdriveruniversity homepage", async () => {
+    console.log("Navigating to the homepage");
+});
+
+When("I click on the contact us button", async () => {
+    console.log("Clicking on the contact us button");
+});
