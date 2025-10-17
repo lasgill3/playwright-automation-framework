d51bd7c5d6800794b1e31ab9bc60cbda44555410
LA-DESKTOP\lasgi <lasgill@gmail.com>
2025-10-17 16:31:47 -0400

    increased timeoout and fisished steps for contact us

--- src/step-definitions/ContactUs_Steps.ts
+++ src/step-definitions/ContactUs_Steps.ts
@@ -1,5 +1,6 @@
 import { defineStep as And, Given, When, Then, Before, After } from '@cucumber/cucumber';
 import { pageFixture } from './hooks/browserContextFixture';
+import { expect } from '@playwright/test';
 
 And('I type a first name', async () => {
     // const firstNameInput = await page.locator('input[name="first_name"]');  
@@ -9,22 +10,23 @@ And('I type a first name', async () => {
 });
 
 And('I type a last name', async () => {
-   await pageFixture.page.locator("input[name='last_name']").fill('Latunji'); 
+   await pageFixture.page.locator("input[name='last_name']").fill('Asgill'); 
 });
 
 And('I enter an email address', async () => {
-   
-
+    await pageFixture.page.locator("input[name='email']").fill('lasgill@gmail.com');
 });
 
 And('I type a comment', async () => {
+    await pageFixture.page.locator("textarea[name='message']").fill('This is a test comment for the contact us form.');
 
 });
 
 And('I click on the submit button', async () => {
-
+    await pageFixture.page.locator("input[type='submit']").click();
 });
 
 Then('I should be presented with a successful contact us submission message', async () => {
-
+    const successMessage = await pageFixture.page.locator("#contact_reply h1");
+    await expect(successMessage).toHaveText("Thank You for your Message!");
 });
--- src/utils/cucumber-timeout.ts
+++ src/utils/cucumber-timeout.ts
@@ -1,3 +1,3 @@
 import { setDefaultTimeout } from "@cucumber/cucumber"; 
 
-setDefaultTimeout(6000); // Set default step timeout to 60 seconds
+setDefaultTimeout(60000); // Set default step timeout to 60 seconds
