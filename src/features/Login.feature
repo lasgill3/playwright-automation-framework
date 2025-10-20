09ce9af821259d7cbc6de6639c52cb3835cbd7a2
LA-DESKTOP\lasgi <lasgill@gmail.com>
2025-10-20 14:29:40 -0400

    created login feature file and steps

---
+++ src/features/Login.feature
@@ -0,0 +1,21 @@
+@login @regression
+Feature: webdriveruniversity - Login Page validation
+
+    # Background: Pre-Conditions
+    #     When I wait for 0 seconds
+
+    Scenario Outline: Valid and Invalid Login Form Submission
+        # Given I navigate to the webdriveruniversity homepage
+        # When I click on the login portal link
+        Given I navigate to the webdriveruniversity login page
+        And I type an username '<username>' and password '<password>'
+        And I click on the login button
+        Then I should be presented with an alert box which contains text '<expectedAlertMessage>'
+        When I click on the alert ok button
+
+        Examples:
+            | username  | password     | expectedAlertMessage |
+            | webdriver | webdriver123 | validation succeeded |
+            | webdriver | invalid      | validation failed    |
+            | joe       | pass12345    | validation failed    |
+
---
+++ src/step-definitions/Login_Steps.ts
@@ -0,0 +1,27 @@
+import { defineStep as And, Given, When, Then } from '@cucumber/cucumber';
+import { pageFixture } from './hooks/browserContextFixture';
+import { expect } from '@playwright/test';
+
+Given('I navigate to the webdriveruniversity login page', async () => {
+    await pageFixture.page.goto('https://www.webdriveruniversity.com/Login-Portal/index.html'); 
+});
+
+And('I type an username {string} and password {string}', async (username: string, pwd: string) => {
+    await pageFixture.page.fill('#text', username);
+    await pageFixture.page.fill('#password', pwd);
+}); 
+
+And('I click on the login button', async () => {
+    await pageFixture.page.click('#login-button');
+});
+
+Then('I should be presented with an alert box which contains text {string}', async (expectedAlertMessage: string) => {
+    pageFixture.page.on('dialog', async dialog => {
+        expect(dialog.message()).toBe(expectedAlertMessage);
+        await dialog.dismiss();
+    });
+}); 
+
+When('I click on the alert ok button', async () => {
+    // The alert is handled in the previous step
+});
