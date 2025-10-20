3f3385af3493128308f62eea7720a27806f71243
LA-DESKTOP\lasgi <lasgill@gmail.com>
2025-10-20 16:31:01 -0400

    added logic to get alert box text and assert that is matches data table

--- src/step-definitions/Login_Steps.ts
+++ src/step-definitions/Login_Steps.ts
@@ -2,6 +2,8 @@ import { defineStep as And, Given, When, Then } from '@cucumber/cucumber';
 import { pageFixture } from './hooks/browserContextFixture';
 import { expect } from '@playwright/test';
 
+let alertText: string;
+
 Given('I navigate to the webdriveruniversity login page', async () => {
     await pageFixture.page.goto('https://www.webdriveruniversity.com/Login-Portal/index.html'); 
 });
@@ -12,14 +14,21 @@ And('I type an username {string} and password {string}', async (username: string
 }); 
 
 And('I click on the login button', async () => {
+    await  pageFixture.page.on('dialog', async (alert) => {
+        alertText = alert.message();
+        // console.log(`Alert message: ${alertText}`);
+        await alert.accept();
+        // await alert.dismiss();
+    });
     await pageFixture.page.click('#login-button', {force: true });
 });
 
 Then('I should be presented with an alert box which contains text {string}', async (expectedAlertMessage: string) => {
-    pageFixture.page.on('dialog', async dialog => {
-        expect(dialog.message()).toBe(expectedAlertMessage);
-        await dialog.dismiss();
-    });
+    // pageFixture.page.on('dialog', async dialog => {
+    //     expect(dialog.message()).toBe(expectedAlertMessage);
+    //     await dialog.dismiss();
+    // });
+    expect(alertText).toEqual(expectedAlertMessage);
 }); 
 
 When('I click on the alert ok button', async () => {
