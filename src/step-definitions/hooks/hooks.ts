9a0142d40c2a584a8f36d3e9fbe472113a1ff2c3
LA-DESKTOP\lasgi <lasgill@gmail.com>
2025-10-21 10:00:56 -0400

    added screenshot to our after hook

--- src/step-definitions/hooks/hooks.ts
+++ src/step-definitions/hooks/hooks.ts
@@ -1,6 +1,7 @@
-import { After, AfterAll, Before, BeforeAll } from "@cucumber/cucumber";
+import { After, AfterAll, Before, BeforeAll, Status } from "@cucumber/cucumber";
 import {Browser, chromium} from "@playwright/test";
 import { pageFixture } from "./browserContextFixture";
+
 
 let browser: Browser;
 
@@ -22,7 +23,20 @@ Before(async function() {
 });
 
 // After hook: Runs after each scenario
-After(async function() {
+After(async function({pickle, result}) {
+    if(result?.status === Status.FAILED) {
+        if(pageFixture.page) {
+            const screenshotPath = `./reports/screenshots/${pickle.name}-${Date.now()}.png`;
+            const image = await pageFixture.page.screenshot({
+                path : screenshotPath,
+                type : 'png',
+                // timeout : 60000
+            });
+            await this.attach(image, 'image/png');
+        } else {
+            console.error("pageFIxture.page is undefined");
+        }
+    }   
     await pageFixture.page.close();
     await browser.close();
 });
