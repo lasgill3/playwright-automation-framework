347e8dc5f1f617d2c0a55ea8f8260c75be200b1c
LA-DESKTOP\lasgi <lasgill@gmail.com>
2025-10-20 22:51:45 -0400

    adding html reporting

--- src/index.ts
+++ src/index.ts
@@ -4,7 +4,9 @@ import { exec } from "child_process";
 const common = `./src/features/*.feature \
   --require-module ts-node/register \
   --require ./src/step-definitions/**/**/*.ts \
-  --require ./src/utils/cucumber-timeout.ts
+  --require ./src/utils/cucumber-timeout.ts \
+  -f json:./reports/report.json \
+  --format html:./reports/report.html \
   --tags "not @ignore" `;
 
 //Define an interface for the profiles object
