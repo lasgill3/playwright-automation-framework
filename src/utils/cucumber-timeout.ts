9e55d93579ee04ac92c3e804b9256bce6d259506
LA-DESKTOP\lasgi <lasgill@gmail.com>
2025-10-22 14:25:02 -0400

    adding timeout variables to env file

--- src/utils/cucumber-timeout.ts
+++ src/utils/cucumber-timeout.ts
@@ -6,4 +6,4 @@ const env = loadEnv({path: './env/.env'});
 
 const customTime = parseInt(env.parsed?.CUCUMBER_STEP_TIMEOUT || '60000'); // Default to 60 seconds if not set  
 
-setDefaultTimeout(60000); // Set default step timeout to 60 seconds
+setDefaultTimeout(customTime); // Set default step timeout to 60 seconds
