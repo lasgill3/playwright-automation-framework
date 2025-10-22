b0bb0a0415946f756b752fe188b66b51b78937fc
LA-DESKTOP\lasgi <lasgill@gmail.com>
2025-10-22 14:16:32 -0400

    adding timeout variables to env file

--- src/utils/cucumber-timeout.ts
+++ src/utils/cucumber-timeout.ts
@@ -1,3 +1,9 @@
 import { setDefaultTimeout } from "@cucumber/cucumber"; 
 
+//Load env variables from .env file
+import {config as loadEnv} from "dotenv";
+const env = loadEnv({path: './env/.env'});
+
+const customTime = parseInt(env.parsed?.CUCUMBER_STEP_TIMEOUT || '60000'); // Default to 60 seconds if not set  
+
 setDefaultTimeout(60000); // Set default step timeout to 60 seconds
--- src/utils/playwright-timeout.ts
+++ src/utils/playwright-timeout.ts
@@ -1,9 +1,16 @@
 import { Page } from "@playwright/test";
 
+//Load env variables from .env file
+import {config as loadEnv} from "dotenv";
+const env = loadEnv({path: './env/.env'});
+
 export function setGlobalTimeouts(page: Page) {
+    const navigationTimeout = parseInt(env.parsed?.UI_AUTOMATION_NAVIGATION_TIMEOUT || '50000');
+    const commandTimeout = parseInt(env.parsed?.UI_AUTOMATION_COMMAND_TIMEOUT || '30000');
+
     //Set Global 'navigation' timeout 
-    page.setDefaultNavigationTimeout(50000); // 50 seconds
+    page.setDefaultNavigationTimeout(navigationTimeout); // 50 seconds  
 
     //Set Global 'command' timeout 
-    page.setDefaultTimeout(30000); // 30 seconds
+    page.setDefaultTimeout(commandTimeout); // 30 seconds
 }
