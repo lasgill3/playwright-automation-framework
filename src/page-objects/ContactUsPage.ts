25d695cc5bf009330a5d0ddf80319a481f5c8827
LA-DESKTOP\lasgi <lasgill@gmail.com>
2025-10-23 17:03:49 -0400

    added methods to my contact us page

--- src/page-objects/ContactUsPage.ts
+++ src/page-objects/ContactUsPage.ts
@@ -1,5 +1,26 @@
 import { BasePage } from "./base/BasePage";
 
-export class ContactUsPage extends BasePage { 
-    
+export class ContactUsPage extends BasePage {
+
+  public async typeFirstName(firstName: string): Promise<void> {
+    await this.page.locator("input[name='first_name']").fill(firstName);
+  }
+
+  public async typeLastName(lastName: string): Promise<void> {
+    await this.page.locator("input[name='last_name']").fill(lastName);
+  }
+
+  public async typeEmailAddress(email: string): Promise<void> {
+    await this.page.locator("input[name='email']").fill(email);
+  }
+
+  public async typeComment(commentMessage: string): Promise<void> {
+    await this.page.locator("textarea[name='message'").fill(commentMessage);
+  }
+
+  public async clickSubmitButton(): Promise<void> {
+     await this.waitAndClickByLocator("input[type='submit']"); 
+  }
+
+
 }
